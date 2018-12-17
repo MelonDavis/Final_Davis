2787ca4d392b3756abe6db219a2fc05f5cbdaa1a
MelonDavis <melina.davis@questu.ca>
2018-12-16 18:48:33 -0800

    Update clean.R

--- Final/clean.data/clean.R
+++ Final/clean.data/clean.R
@@ -11,5 +11,16 @@ inv.d <- read.csv(file.choose("invertebrate"), stringsAsFactors = FALSE,
 
 tail(inv.d) #successfully transformed dashes to NA
 
+#We can see that data has only been entered for quadrats in which there were 
+  #species, however if we want to represent all the data gathered we need to 
+  #include the result of 0 for those quadrats w/ no species found 
+
+#import coordinate data for every quadrat
+coor.d <- 
+
+nrow(inv.d) #118
+ #19
 
 
+inv.d[118 + i,] <- rbind(rep(NA, ncol(inv.d)))
+tail(inv.d)
