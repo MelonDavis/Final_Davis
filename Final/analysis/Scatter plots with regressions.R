f601f397ec390085a80c85162887230958879886
ShanafeltBen <45368288+ShanafeltBen@users.noreply.github.com>
2018-12-18 19:08:11 -0800

    Updated Scatter Plots
    Includes all data now

--- Final/analysis/Scatter plots with regressions.R
+++ Final/analysis/Scatter plots with regressions.R
@@ -4,11 +4,10 @@ wd <- getwd()
 
 
 #load data and replace dashes with "NA"
-Invert <- read.csv("invertebrate_raw_new.csv", na.strings = c("NA", "-"))
+Invert <- read.csv("allquad2.csv", na.strings = c("NA", "-"))
 View(Invert)
 
 #---TOTAL (without B.glandula)-------------------------------------------------
-
 Inv.Phy <- unique(Invert$Phylum)
 col.inv <- c("springgreen", "steelblue1", "tomato", "purple")
 
@@ -24,25 +23,35 @@ for(i in 1:length(Inv.Phy)){
          pch = 19)
   
 }
-#Creating regression lines for each order
+#Creating regression lines for each phylum
 
 inv.interc.t <- inv.slope.t <- rep(NA,length(Inv.Phy))
 
+pv.Inv <- rep(NA, length(Inv.Phy))
 
 for(i in 1:length(Inv.Phy)){
-  #i <- 6
-  # select first Order
+  #i <- 4 #Conduct for loop for a specific point
+  #
   d.inv2 <- Invert[Invert$Phylum == Inv.Phy[i] & 
                      Invert$Number.of.individuals < 177, ]
   m.inv <- lm(d.inv2$Number.of.individuals ~ d.inv2$WE..ft.from.HTL.)
+  #set slope and intercept
   inv.slope.t[i] <- m.inv$coefficients[2]
   inv.interc.t[i] <- m.inv$coefficients[1]
-  summary(m.inv)
-  #adds lines
+  #Extract the p-value of the f statistic calculated by the regression.
+  #accessible in the vector coef.Inv
+  pv.Inv[i] <- pf(summary(m.inv)$fstatistic[1], summary(m.inv)$fstatistic[2],
+                    summary(m.inv)$fstatistic[3], lower.tail = FALSE)
+  #adds regression lines
   abline(a=inv.interc.t[i], b=inv.slope.t[i], col = col.inv[i])
   
 }
-
+#View P-values
+pv.Inv
+#Create P-value by Phylum table
+Invert.by.Phylum.Table <- data.frame("Phylum" = Inv.Phy, "P.value" = pv.Inv)
+#view Table
+View(Invert.by.Phylum.Table)
 
 #Add a legend (Be sure it doesn't cover up points)
 legend(110, 180, Inv.Phy, pch = 19, col = col.inv)
@@ -55,7 +64,7 @@ Mollusca <- Invert[Invert$Phylum == "Mollusca" &
 str(Mollusca)
 #View(Mollusca)
 Mol.Ord <- unique(Mollusca$Order)
-col.mol <- c("springgreen", "steelblue1", "tomato", "yellow2", "purple", 
+col.mol <- c("springgreen", "steelblue1", "tomato", "black", "purple", 
              "navy", "dodgerblue")
 
 #Create a plot for only the Phyllum Mollusca, colors are by order
@@ -66,6 +75,7 @@ plot(NA, xlim = xlim.mol, ylim = ylim.mol, xlab = "Distance from HTL (ft)",
      ylab = "Abundance", main = "Mollusca")
 
 for(i in 1:length(Mol.Ord)){
+  #i <- 4
   d.mol <- Mollusca[Mollusca$Order == Mol.Ord[i], ]
   points(d.mol$WE..ft.from.HTL., d.mol$Number.of.individuals, col=col.mol[i],
          pch = 19)
@@ -75,22 +85,35 @@ for(i in 1:length(Mol.Ord)){
 
 mol.interc.t <- mol.slope.t <- rep(NA,length(Mol.Ord))
 
+pv.mol <- rep(NA, length(Mol.Ord))
 
-for(i in 1:(length(Mol.Ord) - 1)){
-  i <- 6
+for(i in 1:length(Mol.Ord)){
+  #i <- 3
   # select first Order
   d.mol2 <- Mollusca[Mollusca$Order == Mol.Ord[i],]
   m.mol <- lm(d.mol2$Number.of.individuals ~ d.mol2$WE..ft.from.HTL.)
   mol.slope.t[i] <- m.mol$coefficients[2]
   mol.interc.t[i] <- m.mol$coefficients[1]
-  summary(m.mol)
+  #Extract the p-value of the f statistic calculated by the regression.
+  #accessible in the vector coef.Inv
+  pv.mol[i] <- pf(summary(m.mol)$fstatistic[1], summary(m.mol)$fstatistic[2],
+                  summary(m.mol)$fstatistic[3], lower.tail = FALSE)
+  
   #adds lines
   abline(a=mol.interc.t[i], b=mol.slope.t[i], col = col.mol[i])
   
 }
 
+#View P-values
+pv.mol
+#Create P-value by Phylum table
+Mol.by.order <- data.frame("Order" = Mol.Ord, "P.value" = pv.mol)
+#view Table
+View(Mol.by.order)
+
+
 #Add a legend (Be sure it doesn't cover up points)
-legend(130, 150, Mol.Ord, pch = 19, col = col.mol)
+legend(100, 170, Mol.Ord, pch = 19, col = col.mol)
 
 #---ARTHROPODA (withour B.glandula)--------------------------------------------
 Arthropoda.wbg <- Invert[Invert$Phylum == "Arthropoda" & 
@@ -120,6 +143,8 @@ for(i in 1:3){
 
 arthro.interc.wbg <- arthro.slope.wbg <- rep(NA,length(Ord.wbg))
 
+pv.artho.wbg <- rep(NA, length(Ord.wbg))
+
 for(i in 1:length(Ord.wbg)){
   #i <- 3
   # select first Order
@@ -128,9 +153,22 @@ for(i in 1:length(Ord.wbg)){
                        d.arthro.wbg2$WE..ft.from.HTL.)
   arthro.slope.wbg[i] <- m.arthro.wbg$coefficients[2]
   arthro.interc.wbg[i] <- m.arthro.wbg$coefficients[1]
+  #Extract the p-value of the f statistic calculated by the regression.
+  #accessible in the vector coef.Inv
+  pv.artho.wbg[i] <- pf(summary(m.arthro.wbg)$fstatistic[1], 
+                        summary(m.arthro.wbg)$fstatistic[2], 
+                        summary(m.arthro.wbg)$fstatistic[3], 
+                        lower.tail = FALSE)
   #adds lines
   abline(a=arthro.interc.wbg[i], b=arthro.slope.wbg[i], col = col.arthro[i])
 }
+
+#View P-values
+pv.artho.wbg
+#Create P-value by Phylum table
+Artho.wbg.by.order <- data.frame("Order" = Ord.wbg, "P.value" = pv.artho.wbg)
+#view Table
+View(Artho.wbg.by.order)
 
 #Add a legend (Be sure it doesn't cover up points)
 legend(120, 25, Ord, pch = 19, col = col.arthro)
@@ -161,6 +199,8 @@ for(i in 1:3){
 
 arthro.interc.t <- arthro.slope.t <- rep(NA,length(Ord))
 
+pv.artho <- rep(NA, length(Ord))
+
 for(i in 1:length(Ord)){
   #i <- 3
   # select first Order
@@ -168,9 +208,22 @@ for(i in 1:length(Ord)){
   m.arthro <- lm(d.arthro2$Number.of.individuals ~ d.arthro2$WE..ft.from.HTL.)
   arthro.slope.t[i] <- m.arthro$coefficients[2]
   arthro.interc.t[i] <- m.arthro$coefficients[1]
+  #Extract the p-value of the f statistic calculated by the regression.
+  #accessible in the vector coef.Inv
+  pv.artho[i] <- pf(summary(m.arthro)$fstatistic[1], 
+                    summary(m.arthro)$fstatistic[2],
+                    summary(m.arthro)$fstatistic[3], 
+                    lower.tail = FALSE)
   #adds lines
   abline(a=arthro.interc.t[i], b=arthro.slope.t[i], col = col.arthro[i])
 }
+
+#View P-values
+pv.artho
+#Create P-value by Phylum table
+Artho.by.order <- data.frame("Order" = Ord, "P.value" = pv.artho)
+#view Table
+View(Artho.by.order)
 
 #Add a legend (Be sure it doesn't cover up points)
 legend(120, 1500, Ord, pch = 19, col = col.arthro)
@@ -202,6 +255,8 @@ for(i in 1:length(An.Ord)){
 
 an.interc.t <- an.slope.t <- rep(NA,length(An.Ord))
 
+pv.an <- rep(NA, length(An.Ord))
+
 for(i in 1:length(An.Ord)){
   #i <- 2
   #select first Order
@@ -209,11 +264,24 @@ for(i in 1:length(An.Ord)){
   m.an <- lm(d.an2$Number.of.individuals ~ d.an2$WE..ft.from.HTL.)
   an.slope.t[i] <- m.an$coefficients[2]
   an.interc.t[i] <- m.an$coefficients[1]
-  summary(m.an)
+  
+  #Extract the p-value of the f statistic calculated by the regression.
+  #accessible in the vector coef.Inv
+  pv.an[i] <- pf(summary(m.an)$fstatistic[1], 
+                    summary(m.an)$fstatistic[2],
+                    summary(m.an)$fstatistic[3], 
+                    lower.tail = FALSE)
   #adds lines
   abline(a=an.interc.t[i], b=an.slope.t[i], col = col.an[i])
   
 }
+
+#View P-values
+pv.an
+#Create P-value by Phylum table
+Annelida.by.order <- data.frame("Order" = An.Ord, "P.value" = pv.an)
+#view Table
+View(Annelida.by.order)
 
 #Add a legend (Be sure it doesn't cover up points)
 legend(140, 3.5, An.Ord, pch = 19, col = col.an)
@@ -242,10 +310,35 @@ for(i in 1:length(Ech.Ord)){
 }
 
 #Creating regression lines for each order
+ech.interc.t <- ech.slope.t <- rep(NA,length(Ech.Ord))
 
-ech.reg <- lm(Echinodermata$Number.of.individuals ~ 
-                Echinodermata$WE..ft.from.HTL.)
-abline(ech.reg, col = "limegreen")
+pv.ech <- rep(NA, length(Ech.Ord))
+
+for(i in 1:length(An.Ord)){
+  #i <- 2
+  
+  d.ech2 <- Echinodermata[Echinodermata$Order == Ech.Ord[i], ]
+  m.ech <- lm(d.ech2$Number.of.individuals ~ d.ech2$WE..ft.from.HTL.)
+  ech.slope.t[i] <- m.an$coefficients[2]
+  ech.interc.t[i] <- m.an$coefficients[1]
+  
+  #Extract the p-value of the f statistic calculated by the regression.
+  #accessible in the vector coef.Inv
+  pv.ech[i] <- pf(summary(m.ech)$fstatistic[1], 
+                 summary(m.ech)$fstatistic[2],
+                 summary(m.ech)$fstatistic[3], 
+                 lower.tail = FALSE)
+  #adds lines
+  abline(a=ech.interc.t[i], b=ech.slope.t[i], col = col.ech[i])
+  
+}
+
+#View P-values
+pv.ech
+#Create P-value by Phylum table
+Echinodermata.by.order <- data.frame("Order" = Ech.Ord, "P.value" = pv.ech)
+#view Table
+View(Echinodermata.by.order)
 
 #Add a legend (Be sure it doesn't cover up points)
-legend(130, 10, Ech.Ord, pch = 19, col = Ech.Ord)
+legend(110, 8, Ech.Ord, pch = 19, col = Ech.Ord)
